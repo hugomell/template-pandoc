39d0f38dc0d14814e42c2d172ccdf5c6d8ec3f25
Hugo Mell <hugo76m@hotmail.com>
2024-12-26 19:56:41 +0100

    fix: annotate filter that works for basic codeblocks syntax
    The `annotate.lua`filter now adds a code block title with either the id
    for the code block or the name of the source file. This works with the
    `dark.css` stylesheet and can de used in the markdown source files
    as follows:
    - for codeblocks with an id:
    
    ::: {.named-cbl}
    []{#<id>}
    
    ```python
    
    ```
    :::
    
    - for codeblocks with a file attribute:
    
    ::: {.named-cbl}
    []{file="src/<script>.py"}
    
    ```python
    
    ```
    :::

--- template/docs/filters/annotate.lua
+++ template/docs/filters/annotate.lua
@@ -1,19 +1,35 @@
-function code_name(block)
-    if block.attributes["file"] ~= nil then
-        return "file:" .. block.attributes["file"]
-    end
-    if block.identifier == nil or block.identifier == "" then
-        return nil
-    else
-        return "«" .. block.identifier .. "»"
-    end
+local function code_block_info(elem)
+    local name = nil
+    local code_block_text = nil
+    elem = elem:walk{
+        Span = function (span)
+            if span.attributes["file"] ~= nil then
+              name = pandoc.Str("file: " .. span.attributes["file"])
+              return {}
+            end
+            if span.identifier ~= nil then
+              name = pandoc.Str("«" .. span.identifier .. "»")
+              return {}
+            end
+        end,
+        CodeBlock = function (block)
+            code_block_text = pandoc.CodeBlock(block.text,
+                                               {class = block.classes[1]})
+            return {}
+        end
+    }
+    local t = {}
+    t["name"] = name
+    t["content"] = code_block_text
+    return t
 end
 
-function CodeBlock(block)
-    local name = code_name(block)
-    if name ~= nil then
-        return pandoc.Div({ pandoc.Div({ pandoc.Str(name) }, {class = "code-block-title"})
-                          , pandoc.CodeBlock(block.text, {class = block.classes[1]})
-                          }, { class="named-code-block" })
+function Div (elem)
+    if elem.classes[1] ~= "named-cbl" then
+        return
     end
+    local info = code_block_info(elem)
+    return pandoc.Div({ pandoc.Div({ info.name }, {class = "code-block-title"})
+                          , info.content
+                      }, { class="named-code-block" })
 end
